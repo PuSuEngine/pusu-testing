1c39de9101d92d255078ad9e77f80be1a73e503f
Janne Enberg <janne.enberg@lietu.net>
2016-07-05 11:20:46 +0300

    Updated to PyPuSu 1.0.1 and refactored python backend for some simple autoreconnect logic

--- python-backend/backend.py
+++ python-backend/backend.py
@@ -1,27 +1,41 @@
-from pypusu.threaded import PuSuClient
+from pypusu.threaded import PuSuClient, PyPuSuConnectionError
 import settings
 from tornado import ioloop
-from time import time
+from time import time, sleep
+
+
+def get_pusu_client(authorization=""):
+    client = PuSuClient(settings.PUSUD)
+    client.authorize(authorization)
+    return client
 
 
 if __name__ == "__main__":
-    client = PuSuClient(settings.PUSUD)
-    client.authorize("")
+    client = get_pusu_client()
 
     def tick():
+        global client
         ts = str(time())
         print("Tick... {}".format(ts))
-        client.publish("time.py", ts)
+        retries = 5
+        while retries > 0:
+            try:
+                client.publish("time.py", ts)
+                break
+            except PyPuSuConnectionError:
+                print("Connection error, retrying...")
+                sleep(1)
+                client = get_pusu_client()
+                retries -= 1
+
 
     print("Setting up IOLoop")
     loop = ioloop.IOLoop.instance()
     print("Setting up tick")
-    pc = ioloop.PeriodicCallback(tick, 25)
+    pc = ioloop.PeriodicCallback(tick, 250)
     print("Starting up tick")
     pc.start()
     print("Starting loop")
     loop.start()
 
     print("Done...")
-
-
